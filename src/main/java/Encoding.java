375708b7fd14a17a3e49bd0c29c7cbff04ca0a95
violetauko <aukoviolet24@gmail.com>
2022-04-22 22:28:48 +0300

    setter and getter for text

--- src/main/java/Encoding.java
+++ src/main/java/Encoding.java
@@ -1,4 +1,11 @@
 public class Encoding {
+  private String text ="";
 
+    public void setText(String text) {
+        this.text = text;
+    }
 
+    public String getText() {
+        return text;
+    }
 }
